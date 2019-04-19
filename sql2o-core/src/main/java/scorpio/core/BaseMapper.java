6f3be805ad3fad60dbead10e624dbe81dc64ae3d
fishlikewater@126.com <fishlikewater@126.com>
2019-04-19 20:38:58 +0800

    修复mapper save方法

--- sql2o-core/src/main/java/scorpio/core/BaseMapper.java
+++ sql2o-core/src/main/java/scorpio/core/BaseMapper.java
@@ -14,16 +14,32 @@ import java.io.Serializable;
  * 该类主要扩展操作(sql与代码分离，以.sqlmap为后缀的文件来存储sql语句，每句sql以K-V的方式)
  */
 @Slf4j
-public abstract class BaseMapper<T extends BaseModel> extends BaseModel<T> implements Serializable {
+public abstract class BaseMapper<T extends BaseModel> extends Model<T> implements Serializable {
 
     /**
      * 通过对象创建数据
-     *
      * @param t 保存对象
      * @return
      */
     public  Object save(T t) {
-        return t.save();
+        String sql = new InsertModel().setTable(table).setMapping(mapping).getSql();
+        log.debug(sql);
+        Connection conn = BaseUtils.getConn(sql);
+        try {
+            return conn.createQuery(sql).bind(t).executeUpdate().getKey();
+        }finally {
+            close(conn);
+        }
+    }
+    public Object saveIgnoreId(T t){
+        String sql = new InsertModel().setIdName(idName).setIgnoreId(true).setTable(table).setMapping(mapping).getSql();
+        log.debug(sql);
+        Connection conn = BaseUtils.getConn(sql);
+        try {
+            return conn.createQuery(sql).bind(t).executeUpdate().getKey();
+        }finally {
+            close(conn);
+        }
     }
 
     @Override
